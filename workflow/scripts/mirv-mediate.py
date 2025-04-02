1328cc375fc1c8ae94d17cd08e79e35b87a9caf7
cgeady <caryn@geady.com>
2025-04-02 18:55:44 -0400

    refactor: clean-up

--- workflow/scripts/mirv-mediate.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def load_pickle(file_path):
-    with open(file_path, 'rb') as file:
-        return pickle.load(file)
-
-survival_data = load_pickle('../../procdata/SARC021/results_surv.pkl')
-
-surv = survival_data[2]
